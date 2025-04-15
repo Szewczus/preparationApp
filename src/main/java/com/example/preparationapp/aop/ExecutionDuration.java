be8c07d371a13ba5f135acc23680689d6f59dae0
Szewczus <ewus9999@gmail.com>
2025-04-15 09:01:52 +0200

    use AOP

---
+++ src/main/java/com/example/preparationapp/aop/ExecutionDuration.java
@@ -0,0 +1,20 @@
+package com.example.preparationapp.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionDuration {
+
+    @Around("execution(* com.example.preparationapp.controllers.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println("Execution time: " + (end - start) + " ms");
+        return result;
+    }
+}
--- src/main/java/com/example/preparationapp/controllers/AccountUserController.java
+++ src/main/java/com/example/preparationapp/controllers/AccountUserController.java
@@ -3,12 +3,14 @@ package com.example.preparationapp.controllers;
 import com.example.preparationapp.models.UpdatedAccountUser;
 import com.example.preparationapp.entity.AccountUser;
 import com.example.preparationapp.services.AccountUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account-user")
+@Slf4j
 public class AccountUserController {
 
     private final AccountUserService accountUserService;
@@ -29,12 +31,14 @@ public class AccountUserController {
     @PostMapping("/create")
     public ResponseEntity<AccountUser> createUser(@RequestBody AccountUser accountUser){
         AccountUser newAccountUser = accountUserService.saveAccountUser(accountUser);
+        log.info("User created");
         return ResponseEntity.status(HttpStatus.CREATED).body(newAccountUser);
     }
 
     @PutMapping("/updateAllAccountUserData/{id}")
     public ResponseEntity<AccountUser> updateAllAccountUserData(@RequestBody UpdatedAccountUser accountUser, @PathVariable Long id){
         AccountUser updatedAccountUser = accountUserService.updateAllAccountUserData(accountUser, id);
+        log.info("User with id: {} updated", id);
         return ResponseEntity.status(HttpStatus.CREATED).body(updatedAccountUser);
     }
 
@@ -46,6 +50,7 @@ public class AccountUserController {
         }
         accountUser.setSurname(surname);
         accountUserService.saveAccountUser(accountUser);
+        log.info("User with id: {} updated", id);
         return ResponseEntity.status(HttpStatus.CREATED).body(accountUser);
     }
 
@@ -56,6 +61,7 @@ public class AccountUserController {
             return ResponseEntity.notFound().build();
         }
         accountUserService.deleteUser(id);
+        log.info("User with id: {} deleted", id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
